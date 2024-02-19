8cf20888104cb1452ea6a638ff45ef520c92f9b2
oguzbits <oguz.oeztuerk@outlook.de>
2024-02-19 21:00:49 +0100

    add next font to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,21 @@
 import type { Preview } from "@storybook/react";
+import React from "react";
 import "../src/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <main className={fontSans.className}>
+        <Story />
+      </main>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
